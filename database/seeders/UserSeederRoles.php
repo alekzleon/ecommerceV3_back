605cb4f4a4b78eccc2d019a3eba9b9c297d0b3f5
Alejandro León <alekzleon03.aa@gmail.com>
2026-06-23 16:27:59 -0600

    seeder

--- database/seeders/UserSeederRoles.php
+++ database/seeders/UserSeederRoles.php
@@ -27,15 +27,36 @@ class UserSeederRoles extends Seeder
                 continue;
             }
 
-            User::query()->updateOrCreate(
-                ['username' => $roleName],
-                [
-                    'name' => ucfirst(str_replace('_', ' ', $roleName)),
-                    'email' => $roleName . '@cloudishop.mx',
-                    'password' => Hash::make('Password'),
-                    'role_id' => $role->id,
-                ]
-            );
+            $email = $roleName . '@cloudishop.mx';
+            $user = User::query()
+                ->where('username', $roleName)
+                ->orWhere('email', $email)
+                ->first();
+
+            if (!$user) {
+                $user = new User([
+                    'username' => $roleName,
+                    'email' => $email,
+                ]);
+            }
+
+            $emailBelongsToAnotherUser = User::query()
+                ->where('email', $email)
+                ->whereKeyNot($user->getKey())
+                ->exists();
+
+            $user->fill([
+                'name' => ucfirst(str_replace('_', ' ', $roleName)),
+                'username' => $roleName,
+                'password' => Hash::make('Password'),
+                'role_id' => $role->id,
+            ]);
+
+            if (!$emailBelongsToAnotherUser) {
+                $user->email = $email;
+            }
+
+            $user->save();
         }
     }
 }
