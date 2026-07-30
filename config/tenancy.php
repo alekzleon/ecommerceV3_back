ef2d233b8f2d877fc7290571151152cd30d800b0
Alejandro León <alekzleon03.aa@gmail.com>
2026-07-30 17:38:26 -0600

    tenancy

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,8 +16,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
-        'localhost',
+        ...array_filter(array_map(
+            'trim',
+            explode(',', env('TENANCY_CENTRAL_DOMAINS', '127.0.0.1,localhost'))
+        )),
     ],
 
     /**
